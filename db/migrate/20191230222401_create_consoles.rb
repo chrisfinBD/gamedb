a418d27cd130107f0adf379fa14f4cfb1a84f2a6
Christie Finnie <christie.finnie@gmail.com>
2019-12-30 14:28:38 -0800

    Initial files for imdb game clone.

---
+++ db/migrate/20191230222401_create_consoles.rb
@@ -0,0 +1,10 @@
+class CreateConsoles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :consoles do |t|
+      t.string :name
+      t.date :release_date
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20191230222435_create_games.rb
@@ -0,0 +1,12 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.string :title
+      t.date :release_date
+      t.string :rating
+      t.bigint :console_id
+
+      t.timestamps
+    end
+  end
+end
